6858c12c97be4bc3c31d51beeb21f1170779a812
Ryan Roemer <ryan@loose-bits.com>
2016-11-18 14:17:30 -0800

    Start adding skeleton tests.

---
+++ test/index.spec.js
@@ -0,0 +1,7 @@
+describe("index", () => {
+  describe("import", () => {
+    it("should not change with empty options");
+    it("should leave unmatched imports unchanged");
+    it("should replace matched token require");
+  });
+});
