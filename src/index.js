c11c5b9557143db671b9da1e80fa7b011d1ad4cb
Ryan Roemer <ryan@loose-bits.com>
2016-11-18 16:34:00 -0800

    Start rewriting stuff.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,39 @@
-export default "TODO";
+
+export default function ({ types: t }) {
+  return {
+    visitor: {
+      // import _ from "TOKEN/lodash"
+      ImportDeclaration(path, state) {
+        const src = path.node.source.value;
+        const opts = state.opts;
+
+        // TODO: LIKELY IGNORE SO WE _JUST_ REWRITE THE REQUIRE.
+
+        console.log("TODO HERE import -> source", src, opts); // eslint-disable-line
+      },
+
+      // require("TOKEN/lodash")
+      CallExpression(path, state) {
+        const node = path.node;
+        const args = node.arguments || [];
+        const opts = state.opts;
+
+        if (node.callee.name === "require" && args.length === 1 && t.isStringLiteral(args[0])) {
+          const src = args[0].value;
+          const [first, ...rest] = src.split("/");
+          const mod = (rest || []).join("/");
+
+          // Parse options and mutate with first match.
+          const replace = opts[Object.keys(opts).find((k) => k === first)];
+          if (replace) {
+            // Rewrite the `require`, then the module argument.
+            node.callee.name = "TODO"; // really need ast rewrite from source.
+            args[0].value = mod;
+          }
+
+          console.log("TODO HERE require -> source", src, replace, mod); // eslint-disable-line
+        }
+      }
+    }
+  };
+}
--- test/index.spec.js
+++ test/index.spec.js
@@ -1,7 +1,34 @@
+const babel = require("babel-core");
+const pluginPath = require.resolve("../lib/index");
+
+const getBabelOps = (opts) => {
+  return {
+    "presets": ["es2015"],
+    "plugins": [
+      [pluginPath, opts]
+    ]
+  };
+};
+
 describe("index", () => {
+  it("TODO REMOVE", () => {
+    // eslint-disable-next-line
+    const trans = babel.transform(`import _ from "FOO_TOKEN/lodash"`, getBabelOps({
+      FOO_TOKEN: "foo"
+    }));
+    console.log(`\n\n${trans.code}\n\n`); // eslint-disable-line
+  });
+
   describe("import", () => {
     it("should not change with empty options");
     it("should leave unmatched imports unchanged");
+    it("should replace matched token require");
+  });
+
+  describe("require", () => {
+    it("should not change with empty options");
+    it("should not change require.resolve even with token");
+    it("should leave unmatched requires unchanged");
     it("should replace matched token require");
   });
 });
